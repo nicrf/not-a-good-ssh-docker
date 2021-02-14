39c867f9facf3f38a4e01538d042b4656542e9d8
Nicolas <nic_rf@hotmail.com>
2021-02-14 16:53:00 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eux
 
-sshpass -p ${TARGET_PASSWORD} ssh -N -v -o StrictHostKeyChecking=no -R ${TARGET_PORT}:${SOURCE_HOST}:${SOURCE_PORT} ${TARGET_USER}@${TARGET_HOST} 
-
-while true; do sleep 1; done 
+while true
+do
+  sshpass -p ${TARGET_PASSWORD} ssh -N -v -o StrictHostKeyChecking=no -R ${TARGET_PORT}:${SOURCE_HOST}:${SOURCE_PORT} ${TARGET_USER}@${TARGET_HOST} 
+  sleep ${KEEP_ALIVE_TIME}
+done

1114b725016f75c3a5057021e974ffdae3f8d413
Nicolas <nic_rf@hotmail.com>
2021-04-13 17:13:46 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -eux
 
 while true
 do
-  sshpass -p ${TARGET_PASSWORD} ssh -N -v -o StrictHostKeyChecking=no -R ${TARGET_PORT}:${SOURCE_HOST}:${SOURCE_PORT} ${TARGET_USER}@${TARGET_HOST} 
+  sshpass -p ${TARGET_PASSWORD} ssh -N -v -o StrictHostKeyChecking=no -o ServerAliveInterval=30 -o ServerAliveCountMax=3 -R ${TARGET_PORT}:${SOURCE_HOST}:${SOURCE_PORT} ${TARGET_USER}@${TARGET_HOST} 
   sleep ${KEEP_ALIVE_TIME}
 done
